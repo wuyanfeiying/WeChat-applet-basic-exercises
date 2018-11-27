38b8f487e016fa6adf629a269eb7a5b27ac3b9b1
wuyanfeiying <wuyanfeiying@163.com>
2018-11-27 17:28:16 +0800

    2018-11-27 17:27:57 案例-评测小程序首页

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      '../../images/2.jpg',
+      '../../images/4.jpg',
+    ],
+    contentItems:['','','',''],
+    listItems: ['', '', '', '','', '', ],
+  },
+})
